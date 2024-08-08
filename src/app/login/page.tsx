79b7939e0092cf908a2122cc04b72e5e3b04c70f
JibranMachiavelli <jibranind31@gmail.com>
2024-08-08 00:32:10 -0300

    toastfy front, revisar.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,20 @@
+// Login.tsx
 'use client';
 
+import React, { useEffect } from 'react';
+import { useToast } from '../../hooks/useToastfy'; // Ajuste o caminho conforme necessário
+
 export default function Login() {
+  const { success, error } = useToast();
+
+  useEffect(() => {
+    success('Uhull');
+    error('Hum');
+  }, [success, error]);
+
   return (
-    <h1>Login</h1>
+    <>
+      <h1>Aqui</h1>
+    </>
   );
 }
---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,7 @@
+'use cliente';
+
+export default function Loading() {
+  return (
+    <span className="loading loading-ring loading-lg"></span>
+  );
+}
